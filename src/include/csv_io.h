1ccfbba33e670dbdb5fac8592cf9f0021e888b08
Rosenya <lajluch@gmail.com>
2026-01-18 21:17:59 +0100

    csv_io.h clean

--- src/include/csv_io.h
+++ src/include/csv_io.h
@@ -1,11 +1,12 @@
 #ifndef CSV_WRITER_H
 #define CSV_WRITER_H
 
-#include "book.h"
 #include "library_state.h"
 
-void save_book_to_csv(FILE *file, Book *book);
-
 void save_library_to_csv(const char *filename, Library_state *library);
+
+int read_file(const char *filename);
+
+void load_csv_file(const char *filename, Library_state *state);
 
 #endif
